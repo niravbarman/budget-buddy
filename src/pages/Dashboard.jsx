56140f0ad411e65f971afef6ec01f0ce45841415
niravbarman <niravbarman@gmail.com>
2024-02-05 16:00:20 -0500

    Implemented some error haandling, login, and updated welcome page to greet user + show create budget form

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,22 +1,66 @@
 //rrd imports
 import { useLoaderData } from "react-router-dom";
 
+//library
+import { toast } from "react-toastify";
+
+//Components
+import Intro from "../components/Intro";
+import AddBudgetForm from "../components/AddBudgetForm";
+
 //Helper functions
-import { fetchData } from "../helpers"
+import { createBudget, fetchData } from "../helpers"
 
 //Loader func
 export function dashboardLoader(){
     const userName = fetchData("userName");
-    return { userName };
+    const budgets = fetchData("budgets");
+    return { userName, budgets };
+}
+
+//action
+export async function dashboardAction({request}) {
+    const data = await request.formData();
+    const {_action, ...values} = Object.fromEntries(data);
+    if(_action === "newUser") {
+        try {
+            localStorage.setItem("userName", JSON.stringify(values.userName));
+            return toast.success(`Welcome, ${values.userName}`);
+        } catch (e) {
+            throw new Error("There was a problem creating your account.")
+        }
+    }
+    if(_action === "createBudget") {
+        try {
+            createBudget( {
+                name: values.newBudget,
+                amount: values.newBudgetAmount
+            })
+            return toast.success("Budget created");
+        } catch (e) {
+            throw new Error("There was a problem creating your budget.")
+        }
+    }
 }
 
 const Dashboard = () => {
-    const { userName } = useLoaderData()
+    const { userName, budgets } = useLoaderData()
     return (
-        <div>
-            <h1>{userName}</h1>
-            Dashboard
-        </div>
+        <>
+            {userName ? (
+                <div className="dashboard">
+                    <h1>Welcome back, <span className="accent">{userName}</span></h1>
+                    <div className="grid-sm">
+                        {/* {budgets ? () : ()} */}
+                        <div className="grid-lg">
+                            <div className="flex-lg">
+                                <AddBudgetForm />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                ) : <Intro />}
+        </>
     )
 }
 
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,26 @@
+//rrd imports
+import { Link, useNavigate, useRouteError } from "react-router-dom"
+
+//library
+import { ArrowUturnLeftIcon, HomeIcon } from "@heroicons/react/24/solid";
+
 const Error = () => {
+    const error = useRouteError()
+    const navigate = useNavigate();
     return (
-        <div>
-            Errorio
+        <div className="error">
+            <h1>We ran into a problem :(</h1>
+            <p>{error.message || error.statusText}</p>
+            <div className="flex-md">
+                <button className="btn btn--dark" onClick={() => navigate(-1)}>
+                    <ArrowUturnLeftIcon width={20} />
+                    <span>Go back</span>
+                </button>
+                <Link to="/" className="btn btn--dark">
+                    <HomeIcon width={20} />
+                    <span>Go home</span>
+                </Link>
+            </div>
         </div>
     )
 }
