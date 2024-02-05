4e2b7c90a62706f284290c992a0b4bf0f5416736
niravbarman <niravbarman@gmail.com>
2024-02-05 17:51:15 -0500

    Added ability to add expenses and see existing budgets

--- src/helpers.js
+++ src/helpers.js
@@ -24,7 +24,48 @@ export const createBudget = ({
     return localStorage.setItem("budgets", JSON.stringify([...existingBudgets, newItem]));
 }
 
+//create expense
+export const createExpense = ({
+    name, amount, budgetId
+}) => {
+    const newItem = {
+        id: crypto.randomUUID(),
+        name: name,
+        createdAt: Date.now(),
+        amount: +amount,
+        budgetId: budgetId
+    }
+    const existingExpenses = fetchData("expenses") ?? [];
+    return localStorage.setItem("expenses", JSON.stringify([...existingExpenses, newItem]));
+}
+
 //delete item
 export const deleteItem = ({key}) => {
     localStorage.removeItem(key);
+}
+
+//calculate budget spent
+export const calculateSpentByBudget = (budgetId) => {
+    const expenses = fetchData("expenses") ?? [];
+    const budgetSpent = expenses.reduce((acc, expense) => {
+        if(expense.budgetId !== budgetId) return acc;
+        return acc += expense.amount
+    }, 0)
+    return budgetSpent;
+}
+
+//Format currency
+export const formatCurrency = (amt) => {
+    return amt.toLocaleString(undefined, {
+        style: "currency",
+        currency: "USD"
+    })
+}
+
+//format percentages
+export const formatPercentage = (amt) => {
+    return amt.toLocaleString(undefined, {
+        style: "percent",
+        minimumFractionDigits: 0
+    })
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,9 +7,11 @@ import { toast } from "react-toastify";
 //Components
 import Intro from "../components/Intro";
 import AddBudgetForm from "../components/AddBudgetForm";
+import AddExpenseForm from "../components/AddExpenseForm";
+import BudgetItem from "../components/BudgetItem";
 
 //Helper functions
-import { createBudget, fetchData } from "../helpers"
+import { createBudget, createExpense, fetchData } from "../helpers"
 
 //Loader func
 export function dashboardLoader(){
@@ -41,6 +43,18 @@ export async function dashboardAction({request}) {
             throw new Error("There was a problem creating your budget.")
         }
     }
+    if(_action === "createExpense") {
+        try {
+            createExpense( {
+                name: values.newExpense,
+                amount: values.newExpenseAmount,
+                budgetId: values.newExpenseBudget
+            })
+            return toast.success(`Expense ${values.newExpense} created!`);
+        } catch (e) {
+            throw new Error("There was a problem creating your expense.")
+        }
+    }
 }
 
 const Dashboard = () => {
@@ -51,12 +65,28 @@ const Dashboard = () => {
                 <div className="dashboard">
                     <h1>Welcome back, <span className="accent">{userName}</span></h1>
                     <div className="grid-sm">
-                        {/* {budgets ? () : ()} */}
-                        <div className="grid-lg">
-                            <div className="flex-lg">
-                                <AddBudgetForm />
+                        {
+                            budgets && budgets.length > 0 ? (
+                            <div className="grid-lg">
+                                <div className="flex-lg">
+                                    <AddBudgetForm />
+                                    <AddExpenseForm budgets={budgets} />
+                                </div>
+                                <h2>Existing Budgets</h2>
+                                <div className="budgets">
+                                    {
+                                        budgets.map((budget) => (<BudgetItem key={budget.id} budget={budget} />))
+                                    }
+                                </div>
                             </div>
-                        </div>
+                            ) : (
+                                <div className="grid-sm">
+                                    <p>Personal budgeting is the secret to financial freedom.</p>
+                                    <p>Create a budget to get started!</p>
+                                    <AddBudgetForm />
+                                </div>
+                            )
+                        }
                     </div>
                 </div>
                 ) : <Intro />}
